3119b73ca9cae7e5bb33a71f0d7ce72cd74c02de
= <=>
2024-03-13 16:04:06 +0200

    init project

---
+++ study/src/main/java/com/ann/study/StudyPlatformApplication.java
@@ -0,0 +1,13 @@
+package com.ann.study;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudyPlatformApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StudyPlatformApplication.class, args);
+	}
+
+}
